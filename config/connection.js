bed07c9d11adcfad3e5cbd76959f3f7a76b5df5e
unknown <steveweed1979@gmail.com>
2022-10-13 12:01:05 -0500

    Corrected boilerplate

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/mygroceryDB', {
+mongoose.connect("mongodb://127.0.0.1:27017/mysocialDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
+});
 
-// Export connection 
 module.exports = mongoose.connection;
